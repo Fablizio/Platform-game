901439b74b5286e96bfaf9710621f45671da2a01
Fablizio <gats200@hotmail.it>
2021-11-20 12:24:13 +0100

    small fix

--- js/script.js
+++ js/script.js
@@ -46,12 +46,14 @@ const checkDead = setInterval(() => {
   if (blockLeft < 20 && blockLeft > -20 && characterTop >= 130) {
       block.style.animation = "none";
       document.getElementById("GameOver").innerHTML = "Game Over. score: " + Math.floor(counter / 100);
-    //   alert("Game Over. score: " + Math.floor(counter / 100));
     let newGame = prompt ("New Game? Y/N")
     if (newGame === "N") {
         window.location.replace("https://edgemony.com/");
     }
-
+    else {
+      counter = 0;
+    }
+   
       block.style.animation = "block 1s infinite linear";
   } else {
       counter++;
